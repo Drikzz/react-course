d2a5e034933f0289b2fa9f2a6ff3cc7c53abbbc2
Drikzz <rikzsuarez@gmail.com>
2026-06-29 11:23:41 +0800

    7e. made the orders-grid component

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -1,11 +1,8 @@
 import axios from "axios";
-import dayjs from "dayjs";
-import { useState, useEffect, Fragment } from "react";
-import { Link } from "react-router";
+import { useState, useEffect } from "react";
 import { Header } from "../../components/Header";
-import BuyAgainIcon from "../../assets/images/icons/buy-again.png";
-import { formatMoney } from "../../utils/money";
 import "./OrdersPage.css";
+import { OrdersGrid } from "./OrdersGrid";
 
 export function OrdersPage({ cart }) {
   const [orders, setOrders] = useState([]);
@@ -27,71 +24,10 @@ export function OrdersPage({ cart }) {
 
       <Header cart={cart} />
 
-      <div class="orders-page">
-        <div class="page-title">Your Orders</div>
+      <div className="orders-page">
+        <div className="page-title">Your Orders</div>
 
-        <div class="orders-grid">
-          {orders.map((order) => {
-            return (
-              <div key={order.id} class="order-container">
-                <div class="order-header">
-                  <div class="order-header-left-section">
-                    <div class="order-date">
-                      <div class="order-header-label">Order Placed:</div>
-                      <div>{dayjs(order.orderTimeMs).format("MMMM D")}</div>
-                    </div>
-                    <div class="order-total">
-                      <div class="order-header-label">Total:</div>
-                      <div>{formatMoney(order.totalCostCents)}</div>
-                    </div>
-                  </div>
-
-                  <div class="order-header-right-section">
-                    <div class="order-header-label">Order ID:</div>
-                    <div>{order.id}</div>
-                  </div>
-                </div>
-
-                <div class="order-details-grid">
-                  {order.products.map((orderProduct) => {
-                    return (
-                      <Fragment key={orderProduct.product.id}>
-                        <div class="product-image-container">
-                          <img src={orderProduct.product.image} />
-                        </div>
-
-                        <div class="product-details">
-                          <div class="product-name">{orderProduct.product.name}</div>
-                          <div class="product-delivery-date">
-                            Arriving on:{" "}
-                            {dayjs(orderProduct.estimatedDeliveryTimeMs).format(
-                              "MMMM D",
-                            )}
-                          </div>
-                          <div class="product-quantity">
-                            Quantity: {orderProduct.quantity}
-                          </div>
-                          <button class="buy-again-button button-primary">
-                            <img class="buy-again-icon" src={BuyAgainIcon} />
-                            <span class="buy-again-message">Add to Cart</span>
-                          </button>
-                        </div>
-
-                        <div class="product-actions">
-                          <Link to="/tracking">
-                            <button class="track-package-button button-secondary">
-                              Track package
-                            </button>
-                          </Link>
-                        </div>
-                      </Fragment>
-                    );
-                  })}
-                </div>
-              </div>
-            );
-          })}
-        </div>
+        <OrdersGrid orders={orders} />
       </div>
     </>
   );
