6497f5d61270e527b7483cb9197c9ccb6ef316ee
Drikzz <rikzsuarez@gmail.com>
2026-08-04 12:28:45 +0800

    8b. Separated the code that reloads the payment summary [cart]

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -12,14 +12,21 @@ export function CheckoutPage({ cart, loadCart }) {
   useEffect(() => {
 
     const fetchCheckoutData = async () => {
-      let response = await axios.get("/api/delivery-options?expand=estimatedDeliveryTime");
+      const response = await axios.get("/api/delivery-options?expand=estimatedDeliveryTime");
       setDeliveryOptions(response.data);
-
-      response = await axios.get("/api/payment-summary");
-      setPaymentSumamry(response.data);
     };
 
     fetchCheckoutData();
+  }, []);
+
+  useEffect(() => {
+
+    const fetchPaymentSumamry = async () => {
+      const response = await axios.get("/api/payment-summary");
+      setPaymentSumamry(response.data);
+    };
+
+    fetchPaymentSumamry();
   }, [cart]);
 
   return (
